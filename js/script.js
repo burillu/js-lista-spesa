3b86825405f63b49c2dfcad4a9501eedf36efe30
burillu <burillu90@hotmail.it>
2023-10-16 23:14:24 +0200

    improved condition in while

--- js/script.js
+++ js/script.js
@@ -4,13 +4,11 @@ let newItem='';
 let itemArray= [];
 // itemArray.push(item);
 let count= 0;
-while (newItem !== 'stop' || count === 10){    
+while (newItem !== 'stop' && count<30){    
     newItem = prompt ('Inserisci nuovo oggetto nella lista della spesa, quando hai finito inserisci stop');
     if (newItem !== 'stop'){
         itemArray.push(newItem);    
     }
-    console.log(itemArray);
-    console.log(count);
     count++;
     
 }
