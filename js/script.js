b35a649bc72d66f93ee1e468484d174b5f0483ab
burillu <burillu90@hotmail.it>
2023-10-16 23:41:05 +0200

    print ul in html

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
 
-// let item = prompt('isnerisci');
-let newItem='';
-let itemArray= [];
-// itemArray.push(item);
-let count= 0;
-while (newItem !== 'stop' && count<30){    
-    newItem = prompt ('Inserisci nuovo oggetto nella lista della spesa, quando hai finito inserisci stop');
-    if (newItem !== 'stop'){
-        itemArray.push(newItem);    
+
+let newItem = '';
+let itemArray = [];
+
+let count = 0;
+while (newItem !== 'stop' && count < 30) {
+    newItem = prompt('Inserisci nuovo oggetto nella lista della spesa, quando hai finito inserisci stop').toLowerCase();
+    if (newItem !== 'stop') {
+        itemArray.push(newItem);
     }
     count++;
-    
+
 }
 console.log(count);
 console.log(itemArray);
-// console.log(item)
+
 
 const unordList = document.querySelector('.list-group');
-let itemList = document.createElement('li'); 
-itemList.className= 'list-group-item';
 
+
+for (let i = 0; i < itemArray.length; i++) {
+    const eachItem = itemArray[i];
+    let itemList = document.createElement('li');
+    itemList.className = 'list-group-item';
+    itemList.innerHTML = eachItem;
+    unordList.append(itemList);
+    
+
+}
+console.log (unordList);
